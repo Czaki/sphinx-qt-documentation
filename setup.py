cd2871a272f8692b4ffcc6e8e32796934567c71d
Grzegorz Bokota <bokota+github@gmail.com>
2020-02-07 19:03:06 +0100

    upgrade version, upgrade setuptools in test
    remove quotes from python_requires

--- setup.py
+++ setup.py
@@ -8,5 +8,5 @@ setuptools.setup(
     long_description=long_description,
     long_description_content_type="text/markdown",
     packages=setuptools.find_packages(),
-    setup_requires=['setuptools>=36.0']
+    setup_requires=['setuptools>=36.0'],
 )
