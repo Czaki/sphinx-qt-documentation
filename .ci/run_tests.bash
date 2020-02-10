fc357fc6ab8cc215018a1d18085e856e3ee48eba
René Fritze <rene.fritze@wwu.de>
2020-02-10 16:19:34 +0100

    [ci] PyQt5 import in Qt.py fails wo avail libGL
    so for this combination we install libgl mesa

--- .ci/run_tests.bash
+++ .ci/run_tests.bash
@@ -2,6 +2,10 @@
 
 set -eux
 pip install .
+if [ ${1} == "Qt_dot_py__PyQt5" ] ; then
+  # PyQt5 import in Qt.py fails wo avail libGL
+  apt update ; apt install -yq libglu1-mesa
+fi
 if [ ${1} == "no_install" ] ; then
   # This build supposed to fail with an exception, if it doesn't,
   # grep will return nonzero and thereby fail the CI step
