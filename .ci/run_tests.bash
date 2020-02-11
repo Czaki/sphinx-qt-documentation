52450c7a4e380469666c8fc8a21e6c3232fa8db1
René Fritze <47802+renefritze@users.noreply.github.com>
2020-02-11 12:32:14 +0100

    Fix ci fail (#3)
    * [ci] PyQt5 import in Qt.py fails wo avail libGL
    
    so for this combination we install libgl mesa
    
    * [ci] rewire setup to avoid external docker resources

--- .ci/run_tests.bash
+++ .ci/run_tests.bash
@@ -5,7 +5,7 @@ pip install .
 if [ ${1} == "no_install" ] ; then
   # This build supposed to fail with an exception, if it doesn't,
   # grep will return nonzero and thereby fail the CI step
-  ~/.local/bin/sphinx-build -W tests/document01 /tmp/build |& grep -q "RuntimeError"
+  sphinx-build -W tests/document01 /tmp/build |& grep -q "RuntimeError"
 else
-  ~/.local/bin/sphinx-build -W tests/document01 /tmp/build
+  sphinx-build -W tests/document01 /tmp/build
 fi
