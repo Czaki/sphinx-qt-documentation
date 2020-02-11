d2a933cca0c92d27de18c5c74e5851fcc7eeb96c
René Fritze <rene.fritze@wwu.de>
2020-02-11 10:36:19 +0100

    [ci] rewire setup to avoid external docker resources

--- .ci/run_tests.bash
+++ .ci/run_tests.bash
@@ -2,14 +2,10 @@
 
 set -eux
 pip install .
-if [ ${1} == "Qt_dot_py__PyQt5" ] ; then
-  # PyQt5 import in Qt.py fails wo avail libGL
-  apt update ; apt install -yq libglu1-mesa
-fi
 if [ ${1} == "no_install" ] ; then
   # This build supposed to fail with an exception, if it doesn't,
   # grep will return nonzero and thereby fail the CI step
-  ~/.local/bin/sphinx-build -W tests/document01 /tmp/build |& grep -q "RuntimeError"
+  sphinx-build -W tests/document01 /tmp/build |& grep -q "RuntimeError"
 else
-  ~/.local/bin/sphinx-build -W tests/document01 /tmp/build
+  sphinx-build -W tests/document01 /tmp/build
 fi
